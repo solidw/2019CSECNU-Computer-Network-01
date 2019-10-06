b333436df50fe59005769de30fb9837bbb24751d
PayDevD <mrpaydfordev@gmail.com>
2019-10-06 16:02:13 +0900

    init

---
+++ src/ARPDlg.java
@@ -0,0 +1,31 @@
+public class ARPDlg implements BaseLayer {
+    @Override
+    public String GetLayerName() {
+        return null;
+    }
+
+    @Override
+    public BaseLayer GetUnderLayer() {
+        return null;
+    }
+
+    @Override
+    public BaseLayer GetUpperLayer(int nindex) {
+        return null;
+    }
+
+    @Override
+    public void SetUnderLayer(BaseLayer pUnderLayer) {
+
+    }
+
+    @Override
+    public void SetUpperLayer(BaseLayer pUpperLayer) {
+
+    }
+
+    @Override
+    public void SetUpperUnderLayer(BaseLayer pUULayer) {
+
+    }
+}

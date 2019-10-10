8e40e57984c8325eca6389c23d5771141b5ce4f9
PayDevD <mrpaydfordev@gmail.com>
2019-10-10 16:03:28 +0900

    add ARPCash in ARPLayer

--- src/ARPLayer.java
+++ src/ARPLayer.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 public class ARPLayer implements BaseLayer {
+
+    public int nUpperLayerCount = 0;
+    public String pLayerName = null;
+    public BaseLayer p_UnderLayer = null;
+    public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
+
     @Override
     public String GetLayerName() {
         return null;
@@ -27,5 +35,44 @@ public class ARPLayer implements BaseLayer {
     @Override
     public void SetUpperUnderLayer(BaseLayer pUULayer) {
 
+    }
+
+    public static class ARPCash {
+        private static String interfaceName;
+        private static byte[] ipAddress = new byte[4];
+        private static byte[] macAddress = new byte[6];
+        private static boolean status;
+
+        public static String InterfaceName() {
+            return interfaceName;
+        }
+
+        public static byte[] IpAddress() {
+            return ipAddress;
+        }
+
+        public static byte[] MacAddress() {
+            return macAddress;
+        }
+
+        public static boolean Status() {
+            return status;
+        }
+
+        public static void setInterfaceName(String interfaceName) {
+            ARPCash.interfaceName = interfaceName;
+        }
+
+        public static void setIpAddress(byte[] ipAddress) {
+            ARPCash.ipAddress = ipAddress;
+        }
+
+        public static void setMacAddress(byte[] macAddress) {
+            ARPCash.macAddress = macAddress;
+        }
+
+        public static void setStatus(boolean status) {
+            ARPCash.status = status;
+        }
     }
 }

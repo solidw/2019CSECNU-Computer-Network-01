ac5b4ac38d1dd11f2c9b3dd11a544fcc66d027d5
PayDevD <mrpaydfordev@gmail.com>
2019-10-10 17:59:10 +0900

    add ARPHeader and fill base method

--- src/ARPLayer.java
+++ src/ARPLayer.java
@@ -7,34 +7,74 @@ public class ARPLayer implements BaseLayer {
     public BaseLayer p_UnderLayer = null;
     public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
 
+    private static class ARPHeader {
+        byte[] HWtype = new byte[2];
+        byte[] protocol = new byte[2];
+        byte HWLength;
+        byte protLength;
+        byte[] opcode = new byte[2];
+        byte[] srcMac = new byte[6];
+        byte[] srcIp = new byte[4];
+        byte[] dstMac = new byte[6];
+        byte[] dstIp = new byte[4];
+
+        public ARPHeader() {
+            HWtype[1] = (byte)0x01;
+
+            protocol[0] = (byte) 0x80;
+
+            HWLength = 6;
+            protLength = 4;
+        }
+    }
+
+    @Override
+    public boolean Send(byte[] input, int length) {
+        return false;
+    }
+
+    @Override
+    public boolean Receive() {
+        return false;
+    }
+
     @Override
     public String GetLayerName() {
-        return null;
+        return pLayerName;
     }
 
     @Override
     public BaseLayer GetUnderLayer() {
-        return null;
+        if (p_UnderLayer == null)
+            return null;
+        return p_UnderLayer;
     }
 
     @Override
     public BaseLayer GetUpperLayer(int nindex) {
-        return null;
+        if (nindex < 0 || nindex > nUpperLayerCount || nUpperLayerCount < 0)
+            return null;
+        return p_aUpperLayer.get(nindex);
     }
 
     @Override
     public void SetUnderLayer(BaseLayer pUnderLayer) {
-
+        if (pUnderLayer == null)
+            return;
+        p_UnderLayer = pUnderLayer;
     }
 
     @Override
     public void SetUpperLayer(BaseLayer pUpperLayer) {
-
+        if (pUpperLayer == null)
+            return;
+        this.p_aUpperLayer.add(nUpperLayerCount++, pUpperLayer);
     }
 
     @Override
     public void SetUpperUnderLayer(BaseLayer pUULayer) {
-
+        this.SetUpperLayer(pUULayer);
+        pUULayer.SetUnderLayer(this);
     }
 
     public static class ARPCash {
